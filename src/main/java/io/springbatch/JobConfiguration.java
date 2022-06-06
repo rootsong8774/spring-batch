f885f9ea05edf452514a81b461ff461984f2c5cf
rootsong8774 <95426072+rootsong8774@users.noreply.github.com>
2022-06-06 17:24:36 +0900

    Spring batch domain - Job, JobInstance

--- src/main/java/io/springbatch/JobConfiguration.java
+++ src/main/java/io/springbatch/JobConfiguration.java
@@ -3,16 +3,19 @@ package io.springbatch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class DBJobConfiguration {
-    
+public class JobConfiguration {
+
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     
@@ -28,7 +31,7 @@ public class DBJobConfiguration {
     public Step step1() {
         return stepBuilderFactory.get("step1")
             .tasklet((contribution, chunkContext) -> {
-                System.out.println("step1 was executed!");
+                System.out.println("step1 was executed");
                 return RepeatStatus.FINISHED;
             })
             .build();
@@ -38,7 +41,7 @@ public class DBJobConfiguration {
     public Step step2() {
         return stepBuilderFactory.get("step2")
             .tasklet((contribution, chunkContext) -> {
-                System.out.println("step2 was executed!");
+                System.out.println("step2 was executed");
                 return RepeatStatus.FINISHED;
             })
             .build();
